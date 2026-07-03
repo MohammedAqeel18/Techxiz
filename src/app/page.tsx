5f440e21c9f9a0f210defbf87fd900349aa3fd06
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-07-03 20:08:24 +0530

    feat:inital home page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home(){
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to TechXiz!</h1>
-      <p> Techxiz under Construction 🚧🏗️👷🏗️ </p>
+      <p className="text-3xl font-bold text-red-800"> Techxiz under Construction 🚧🏗️👷🏗️ </p>
       <p className="mt-4 text-lg text-gray-600">Talk Tech! Build Web!</p>
 
     </main>
